69a3f3bf5a6ecb5630ed141d672b5fadb6e63719
Iain Foulds <iain@fouldsy.com>
2017-08-27 20:12:44 -0700

    Let's eat

---
+++ app.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var documentClient = require("documentdb").DocumentClient;
+var config = require("./config");
+var url = require('url');
+
+var http = require('http');
+
+var config = require("./config");
+var url = require('url');
+
+var client = new documentClient(config.endpoint, { "masterKey": config.primaryKey });
+
+var databaseUrl = `dbs/${config.database.id}`;
+var collectionUrl = `${databaseUrl}/colls/${config.collection.id}`;
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+
+    response.write('Current read endpoint is: ' + readEndpoint + '\n\n');
+
+    client.queryDocuments(
+            collectionUrl,
+            'SELECT c.description,c.cost FROM c'
+        ).toArray((err, results) => {
+            if (err) reject(err)
+            else {
+                for (var queryResult of results) {
+                    response.write('Name: ' + queryResult.description + '\n');
+                    response.write('Cost: $' + queryResult.cost+ '\n\n');
+                }
+                response.end();
+            }
+        });
+});
+
+var port = process.env.PORT || 3000;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.endpoint = "https://azuremolmememt.documents.azure.com:443/";
+config.primaryKey = "Lad6Ui0n0d1yjoutS41uU2iezCxEKadfxetmQmlOJRNOp3a0qOJwPfIQwNzKEZ7Zf7X9KhnOimtf6aIEQa6HwA==";
+config.database = { "id": "pizzadb" };
+config.collection = { "id": "pizzas" };
+
+module.exports = config;
