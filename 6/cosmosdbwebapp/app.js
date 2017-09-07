85045d41bdb05e33b295756cff78e8b39d7a5dfe
Iain Foulds <iain@fouldsy.com>
2017-09-06 22:49:15 -0700

    Adding geo replication output and code comments

--- 6/cosmosdbwebapp/app.js
+++ 6/cosmosdbwebapp/app.js
@@ -1,38 +1,63 @@
 "use strict";
 
-var documentClient = require("documentdb").DocumentClient;
 var config = require("./config");
 var url = require('url');
 var http = require('http');
 
-
+// Create a connection to Cosmos DB
+// Here, we use the Document model, and define a connection policy
+var documentClient = require("documentdb").DocumentClient;
 var lib = require("./node_modules/documentdb/lib/");
+
+// The connection policy can use automatic endpoint discovery, or you can specify a list
+// of preferred endpoints.
 var connectionPolicy = new lib.DocumentBase.ConnectionPolicy();
-connectionPolicy.EnableEndpointDiscovery = 'True';
 
+// Using automatic discovery is a more dynamic approach, and your code requires no changes
+// as you add or remove endpoints in Cosmos DB
+//connectionPolicy.EnableEndpointDiscovery = 'True';
+
+// Using a list of preferred locations allows you to control the order in which endpoints
+// are used. In our example, we selected 'West Europe' as an additional endpoint. By then
+// setting 'West Europe' as our preferred location, although all writes still go through the
+// the primary endpoint of 'East US', all reads go through 'West Europe'.
+connectionPolicy.PreferredLocations = ['West Europe', 'East US'];
+
+// Now actually make the connection to Cosmos DB using our endpoint, key, and connection policy
 var client = new documentClient(config.endpoint, { "masterKey": config.primaryKey }, connectionPolicy);
-
 var databaseUrl = `dbs/${config.database.id}`;
 var collectionUrl = `${databaseUrl}/colls/${config.collection.id}`;
 
-//client.deleteDatabase(`dbs/${databaseId}`, callback);
-
-var read = documentClient.getReadEndpoint;
-var write = documentClient.getWriteEndpoint;
-
+// Start a basic HTTP server in Node.js for our basic web app
 var server = http.createServer(function(request, response) {
 
+    // Start to write a basic HTTP response to render our page
     response.writeHead(200, {"Content-Type": "text/plain"});
 
-    response.write('Write endpoint is: ' + read);
-    response.write('\nRead endpoint is: ' + read + '\n\n');
+    // Output the current write endpoint
+    // This is the endpoint where all write requests route through to ensure data consistency
+    client.getWriteEndpoint(function(endpoint) {
+        response.write('Current write endpoint is: ' + endpoint + '\n');
+    });
 
+    // Output the current read endpoint
+    // This is the power of Cosmos DB. The APIs determine the closest read endpoint and route your
+    // requests accordingly. There's nothing your app does here.
+    // This is purely for informational purposes. You just query the database in the next code block
+    // and let the APIs determine the most appropriate endpoint to pull data from.
+    client.getReadEndpoint(function(endpoint) {
+        response.write('Current read endpoint is: ' + endpoint + '\n\n');
+    });
+
+    // Query the database to obtain a basic list of pizzas and their costs
+    // This can be expanded to sort by price or alphabetically by pizza name, for example
     client.queryDocuments(
             collectionUrl,
             'SELECT c.description,c.cost FROM c'
         ).toArray((err, results) => {
             if (err) reject(err)
             else {
+
                 for (var queryResult of results) {
                     response.write('Name: ' + queryResult.description + '\n');
                     response.write('Cost: $' + queryResult.cost+ '\n\n');
@@ -42,6 +67,9 @@ var server = http.createServer(function(request, response) {
         });
 });
 
+// Start the web server listening on port 3000
+// If you use Azure Web Apps, this port is mapped back to port 80 so that's all you need to
+// enter in your web browser
 var port = process.env.PORT || 3000;
 server.listen(port);
 
