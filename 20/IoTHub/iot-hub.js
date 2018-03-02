f3b04d2c202d068f195eaae4b4f0b83d9e45ffd0
Iain Foulds <iain@fouldsy.com>
2018-03-01 20:24:17 -0800

    Adding IoT samples in chapter 20

---
+++ 20/IoTHub/iot-hub.js
@@ -0,0 +1,42 @@
+/*
+ * IoT Gateway BLE Script - Microsoft Sample Code - Copyright (c) 2016 - Licensed MIT
+ */
+'use strict';
+
+var EventHubClient = require('azure-event-hubs').Client;
+
+// Close connection to IoT Hub.
+IoTHubReaderClient.prototype.stopReadMessage = function() {
+  this.iotHubClient.close();
+}
+
+// Read device-to-cloud messages from IoT Hub.
+IoTHubReaderClient.prototype.startReadMessage = function(cb) {
+  var printError = function(err) {
+    console.error(err.message || err);
+  };
+
+  this.iotHubClient.open()
+    .then(this.iotHubClient.getPartitionIds.bind(this.iotHubClient))
+    .then(function(partitionIds) {
+      return partitionIds.map(function(partitionId) {
+        return this.iotHubClient.createReceiver(this.consumerGroupName, partitionId, {
+          'startAfterTime': Date.now() - 10000
+        })
+        .then(function(receiver) {
+          receiver.on('errorReceived', printError);
+          receiver.on('message', (message) => {
+            cb(message.body, Date.parse(message.systemProperties['x-opt-enqueued-time']));
+          });
+        }.bind(this));
+      }.bind(this));
+    }.bind(this))
+    .catch(printError);
+}
+
+function IoTHubReaderClient(connectionString, consumerGroupName) {
+  this.iotHubClient = EventHubClient.fromConnectionString(connectionString);
+  this.consumerGroupName = consumerGroupName;
+}
+
+module.exports = IoTHubReaderClient;
