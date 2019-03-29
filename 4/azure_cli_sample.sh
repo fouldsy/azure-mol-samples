e80f0ae530ddb89ddaac3cf1b3ba448549900626
Iain Foulds <iain@fouldsy.com>
2019-03-28 21:45:45 -0700

    [4] Update --disk parameter due to future deprecation
    In `az vm disk attach`, the `--disk` parameter will be deprecated and replaced by `--name`.

--- 4/azure_cli_sample.sh
+++ 4/azure_cli_sample.sh
@@ -62,7 +62,7 @@ esac
 az vm disk attach \
     --resource-group azuremolchapter4 \
     --vm-name storagevm \
-    --disk datadisk \
+    --name datadisk \
     --size-gb 64 \
     --sku Premium_LRS \
     --new
